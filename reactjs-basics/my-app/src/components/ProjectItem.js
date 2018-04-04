a1bac95aaad94a8f719a2166e829509051f37d2f
Tracy Desserich <tdesserich@gmail.com>
2018-04-04 10:11:39 -0400

    updates

---
+++ reactjs-basics/my-app/src/components/ProjectItem.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class ProjectItem extends Component {
+  render() {
+    return (
+      <li className="Projects">
+       
+      </li>
+    );
+  }
+}
+
+export default ProjectItem;
